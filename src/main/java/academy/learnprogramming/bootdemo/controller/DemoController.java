bf8b54e5e1ed95f666682fffb5eca5e44d17e070
serghalak <serghalak@gmail.com>
2020-04-16 11:50:11 +0300

    add DemoController

--- src/main/java/academy/learnprogramming/bootdemo/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/bootdemo/controller/DemoController.java
@@ -1,7 +1,18 @@
 package academy.learnprogramming.bootdemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
+
+    @GetMapping("demo")
+    @ResponseBody
+    public String demo(){
+        log.info("demo method called");
+        return "Hello Spring Boot";
+    }
 }
